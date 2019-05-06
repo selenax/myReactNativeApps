699879845d0fd1372834eab1cfd867fdfee74daf
Selena <selena@kony.com>
2019-05-05 22:02:25 -0700

    config estlint, successfully built ios instance

---
+++ albums/index.ios.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+const App = () => (
+    <Text>Some text</Text>
+);
+
+AppRegistry.registerComponent('albums', () => App);
